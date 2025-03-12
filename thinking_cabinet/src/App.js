b1512c472731855bf53e5574386dbe023ba6efd1
DupieM <221326@virtualwindow.co.za>
2025-03-12 15:01:40 +0200

    Adding navigation bar start of it (Will use bootstrap later))

--- thinking_cabinet/src/App.js
+++ thinking_cabinet/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Authentications from './Pages/Authentication/Authentication';
 import Home from './Pages/Home/Home';
 
@@ -8,27 +8,25 @@ import CabinetAIPre from './Pages/CabinetAI/SetUp/CabinetAI-pre';
 import CabinetAIPost from './Pages/CabinetAI/Reslut/CabinetAI-post';
 import WunderchatPre from './Pages/Wunderchat/Setup/Wunderchat-pre';
 import WunderchatPost from './Pages/Wunderchat/Result/Wunderchat-post';
+import Navbar from './componements/navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          <Route path="/authentication" element={<Authentications />} />
-          <Route path="/" element={<Home />} />
-          
-          <Route path="/wunderchat-pre" element={<WunderchatPre />} />
-          <Route path="/cabinetAI-pre" element={<CabinetAIPre />} />
-          <Route path="/profile" element={<Profile />} />
+      <div>
+          <Navbar />
+          <Routes>
+            <Route path="/authentication" element={<Authentications />} />
+            <Route path="/" element={<Home />} />
 
-          <Route path="/wunderchat-post" element={<WunderchatPost />} />
-          <Route path="/cabinetAI-post" element={<CabinetAIPost />} />
-        </Routes>
+            <Route path="/wunderchat-pre" element={<WunderchatPre />} />
+            <Route path="/cabinetAI-pre" element={<CabinetAIPre />} />
+            <Route path="/profile" element={<Profile />} />
 
-        <h1> The thinking cabinet </h1>
-
-      </header>
-    </div>
+            <Route path="/wunderchat-post" element={<WunderchatPost />} />
+            <Route path="/cabinetAI-post" element={<CabinetAIPost />} />
+          </Routes> 
+      </div>
   );
 }
 
