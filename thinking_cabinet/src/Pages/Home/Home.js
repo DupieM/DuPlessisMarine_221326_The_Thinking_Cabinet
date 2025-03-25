d1fd597391403385bc79e6462554e1dd9c5f6b06
DupieM <221326@virtualwindow.co.za>
2025-03-25 23:06:22 +0200

    adding styling to home page

--- thinking_cabinet/src/Pages/Home/Home.js
+++ thinking_cabinet/src/Pages/Home/Home.js
@@ -21,7 +21,16 @@ function Home() {
 
             <br/>
 
-            <h2 style={{marginLeft: '70px', fontWeight: 'bold', color: 'black', fontSize: '30pt'}}>Home</h2>
+            <h2 style={{marginLeft: '70px', fontWeight: 'bold', color: '#203023', fontSize: '30pt'}}>Home</h2>
+
+            <br/>
+
+            <p style={{marginLeft: '60px', marginRight: '60px', backgroundColor: 'white', color: '#203023'}}>
+              Welcome to The Thinking Cabinet, a unique space for self-reflection, creativity, and discovery. 
+              Here, you can create your own Wunderkammer by uploading pictures, with AI generating narratives to 
+              bring them to life. Engage in questioning these objects via AI and getting thought-provoking 
+              AI-generated answers or crafting your own for deeper reflection
+            </p>
 
             <br/>
 
