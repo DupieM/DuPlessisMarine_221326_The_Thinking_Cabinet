0fe17b8d55f5b05af189fc01c9ff3a5fa4879590
DupieM <221326@virtualwindow.co.za>
2025-04-22 14:22:57 +0200

    adding logo to navbar

--- thinking_cabinet/src/componements/navbar.js
+++ thinking_cabinet/src/componements/navbar.js
@@ -6,6 +6,7 @@ import { auth } from "../firebase";
 import './navbar.css';
 import icon from './avatar.png';
 import icon2 from './logout.png';
+import logo from './LOGO.png'
 
 function BasicNavbar() {
     const [user, setUser] = useState(null);
@@ -32,11 +33,11 @@ function BasicNavbar() {
     return (
       <Navbar variant="dark" className="navbar">
             <Container style={{textAlign: 'left'}}>
-                <Navbar.Brand href="/home" style={{marginRight: "6px"}}>The Thinking Cabinet</Navbar.Brand>
+                <Navbar.Brand href="/home" style={{marginRight: "0px"}}><img  src={logo} style={{width: '35%'}}/></Navbar.Brand>
                 <Nav.Link href="/cabinetAI-pre" style={{fontSize: '18pt', color: '#1B1D13', fontWeight: '700'}}>CabinetAI</Nav.Link>
-                <Navbar.Brand href="/profile"  style={{fontSize: '18pt', color: '#1B1D13', fontWeight: '700', marginLeft: '-750px'}}><img src={icon} style={{width: '13%'}}/></Navbar.Brand>
+                <Navbar.Brand href="/profile"  style={{fontSize: '18pt', color: '#1B1D13', fontWeight: '700', marginLeft: '-560px'}}><img src={icon} style={{width: '13%'}}/></Navbar.Brand>
                 {user ? (
-                  <Navbar.Brand onClick={handleLogout} style={{marginRight: '-150px', marginLeft: '-985px'}}><img src={icon2} style={{width: '14%'}}/></Navbar.Brand>
+                  <Navbar.Brand onClick={handleLogout} style={{marginRight: '-150px', marginLeft: '-795px'}}><img src={icon2} style={{width: '14%'}}/></Navbar.Brand>
                   ) : (
                   <div></div>
                 )}
